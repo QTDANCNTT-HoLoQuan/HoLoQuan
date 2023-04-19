b3eb14a7ab19a62dd40e6262c2fed2005ded3d4f
Duy317 <duyn1585@gmail.com>
2023-04-19 07:18:16 +0700

    form order

---
+++ HoLoQuan/FormQkyOrder.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace HoLoQuan
+{
+    partial class FormQkyOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonDoiban = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewttb = new System.Windows.Forms.DataGridView();
+            this.txtidb = new System.Windows.Forms.TextBox();
+            this.buttonGiamgia = new System.Windows.Forms.Button();
+            this.cbDoiban = new System.Windows.Forms.ComboBox();
+            this.buttonquaylai = new System.Windows.Forms.Button();
+            this.buttonThanhtoan = new System.Windows.Forms.Button();
+            this.cbGiamgia = new System.Windows.Forms.ComboBox();
+            this.nrSoluong = new System.Windows.Forms.NumericUpDown();
+            this.buttonThemmon = new System.Windows.Forms.Button();
+            this.comboBoxMon = new System.Windows.Forms.ComboBox();
+            this.flpTable = new System.Windows.Forms.FlowLayoutPanel();
+            this.comboBoxLoai = new System.Windows.Forms.ComboBox();
+            this.groupBoxBan = new System.Windows.Forms.GroupBox();
+            this.dANHMUCBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewttb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nrSoluong)).BeginInit();
+            this.groupBoxBan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dANHMUCBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDoiban
+            // 
+            this.buttonDoiban.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonDoiban.Location = new System.Drawing.Point(759, 584);
+            this.buttonDoiban.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonDoiban.Name = "buttonDoiban";
+            this.buttonDoiban.Size = new System.Drawing.Size(118, 45);
+            this.buttonDoiban.TabIndex = 47;
+            this.buttonDoiban.Text = "Đổi bàn";
+            this.buttonDoiban.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dataGridViewttb);
+            this.groupBox1.Controls.Add(this.txtidb);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(759, 177);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(476, 396);
+            this.groupBox1.TabIndex = 38;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin bàn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "ID Bàn:";
+            // 
+            // dataGridViewttb
+            // 
+            this.dataGridViewttb.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridViewttb.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewttb.Location = new System.Drawing.Point(7, 70);
+            this.dataGridViewttb.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridViewttb.Name = "dataGridViewttb";
+            this.dataGridViewttb.ReadOnly = true;
+            this.dataGridViewttb.RowHeadersWidth = 51;
+            this.dataGridViewttb.RowTemplate.Height = 24;
+            this.dataGridViewttb.Size = new System.Drawing.Size(462, 319);
+            this.dataGridViewttb.TabIndex = 26;
+            // 
+            // txtidb
+            // 
+            this.txtidb.Location = new System.Drawing.Point(91, 30);
+            this.txtidb.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtidb.Name = "txtidb";
+            this.txtidb.Size = new System.Drawing.Size(101, 30);
+            this.txtidb.TabIndex = 24;
+            // 
+            // buttonGiamgia
+            // 
+            this.buttonGiamgia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGiamgia.Location = new System.Drawing.Point(929, 584);
+            this.buttonGiamgia.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonGiamgia.Name = "buttonGiamgia";
+            this.buttonGiamgia.Size = new System.Drawing.Size(118, 45);
+            this.buttonGiamgia.TabIndex = 43;
+            this.buttonGiamgia.Text = "Giảm giá";
+            this.buttonGiamgia.UseVisualStyleBackColor = true;
+            // 
+            // cbDoiban
+            // 
+            this.cbDoiban.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDoiban.FormattingEnabled = true;
+            this.cbDoiban.Location = new System.Drawing.Point(759, 636);
+            this.cbDoiban.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbDoiban.Name = "cbDoiban";
+            this.cbDoiban.Size = new System.Drawing.Size(118, 33);
+            this.cbDoiban.TabIndex = 48;
+            // 
+            // buttonquaylai
+            // 
+            this.buttonquaylai.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonquaylai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonquaylai.Location = new System.Drawing.Point(30, 11);
+            this.buttonquaylai.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonquaylai.Name = "buttonquaylai";
+            this.buttonquaylai.Size = new System.Drawing.Size(138, 52);
+            this.buttonquaylai.TabIndex = 46;
+            this.buttonquaylai.Text = "Quay lại";
+            this.buttonquaylai.UseVisualStyleBackColor = false;
+            // 
+            // buttonThanhtoan
+            // 
+            this.buttonThanhtoan.BackColor = System.Drawing.Color.Silver;
+            this.buttonThanhtoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThanhtoan.Location = new System.Drawing.Point(1087, 601);
+            this.buttonThanhtoan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonThanhtoan.Name = "buttonThanhtoan";
+            this.buttonThanhtoan.Size = new System.Drawing.Size(147, 70);
+            this.buttonThanhtoan.TabIndex = 45;
+            this.buttonThanhtoan.Text = "Thanh toán";
+            this.buttonThanhtoan.UseVisualStyleBackColor = false;
+            // 
+            // cbGiamgia
+            // 
+            this.cbGiamgia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbGiamgia.FormattingEnabled = true;
+            this.cbGiamgia.Items.AddRange(new object[] {
+            "10%",
+            "20%",
+            "50%"});
+            this.cbGiamgia.Location = new System.Drawing.Point(929, 636);
+            this.cbGiamgia.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbGiamgia.Name = "cbGiamgia";
+            this.cbGiamgia.Size = new System.Drawing.Size(118, 33);
+            this.cbGiamgia.TabIndex = 44;
+            // 
+            // nrSoluong
+            // 
+            this.nrSoluong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nrSoluong.Location = new System.Drawing.Point(1031, 110);
+            this.nrSoluong.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.nrSoluong.Name = "nrSoluong";
+            this.nrSoluong.Size = new System.Drawing.Size(64, 30);
+            this.nrSoluong.TabIndex = 42;
+            this.nrSoluong.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonThemmon
+            // 
+            this.buttonThemmon.BackColor = System.Drawing.Color.Silver;
+            this.buttonThemmon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThemmon.Location = new System.Drawing.Point(1112, 81);
+            this.buttonThemmon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonThemmon.Name = "buttonThemmon";
+            this.buttonThemmon.Size = new System.Drawing.Size(123, 89);
+            this.buttonThemmon.TabIndex = 41;
+            this.buttonThemmon.Text = "Thêm món";
+            this.buttonThemmon.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxMon
+            // 
+            this.comboBoxMon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxMon.FormattingEnabled = true;
+            this.comboBoxMon.Location = new System.Drawing.Point(759, 135);
+            this.comboBoxMon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxMon.Name = "comboBoxMon";
+            this.comboBoxMon.Size = new System.Drawing.Size(252, 33);
+            this.comboBoxMon.TabIndex = 40;
+            // 
+            // flpTable
+            // 
+            this.flpTable.AutoScroll = true;
+            this.flpTable.Location = new System.Drawing.Point(7, 31);
+            this.flpTable.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.flpTable.Name = "flpTable";
+            this.flpTable.Size = new System.Drawing.Size(648, 561);
+            this.flpTable.TabIndex = 35;
+            // 
+            // comboBoxLoai
+            // 
+            this.comboBoxLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxLoai.FormattingEnabled = true;
+            this.comboBoxLoai.Location = new System.Drawing.Point(759, 81);
+            this.comboBoxLoai.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxLoai.Name = "comboBoxLoai";
+            this.comboBoxLoai.Size = new System.Drawing.Size(252, 33);
+            this.comboBoxLoai.TabIndex = 39;
+            // 
+            // groupBoxBan
+            // 
+            this.groupBoxBan.Controls.Add(this.flpTable);
+            this.groupBoxBan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxBan.Location = new System.Drawing.Point(47, 81);
+            this.groupBoxBan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBoxBan.Name = "groupBoxBan";
+            this.groupBoxBan.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBoxBan.Size = new System.Drawing.Size(662, 600);
+            this.groupBoxBan.TabIndex = 37;
+            this.groupBoxBan.TabStop = false;
+            this.groupBoxBan.Text = "Bàn";
+            // 
+            // dANHMUCBindingSource
+            // 
+            this.dANHMUCBindingSource.DataMember = "DANHMUC";
+            // 
+            // FormQkyOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HoLoQuan.Properties.Resources.bghongtim;
+            this.ClientSize = new System.Drawing.Size(1325, 728);
+            this.Controls.Add(this.buttonDoiban);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonGiamgia);
+            this.Controls.Add(this.cbDoiban);
+            this.Controls.Add(this.buttonquaylai);
+            this.Controls.Add(this.buttonThanhtoan);
+            this.Controls.Add(this.cbGiamgia);
+            this.Controls.Add(this.nrSoluong);
+            this.Controls.Add(this.buttonThemmon);
+            this.Controls.Add(this.comboBoxMon);
+            this.Controls.Add(this.comboBoxLoai);
+            this.Controls.Add(this.groupBoxBan);
+            this.Name = "FormQkyOrder";
+            this.Text = "FormQkyOrder";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewttb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nrSoluong)).EndInit();
+            this.groupBoxBan.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dANHMUCBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDoiban;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewttb;
+        private System.Windows.Forms.TextBox txtidb;
+        private System.Windows.Forms.Button buttonGiamgia;
+        private System.Windows.Forms.BindingSource dANHMUCBindingSource;
+        private System.Windows.Forms.ComboBox cbDoiban;
+        private System.Windows.Forms.Button buttonquaylai;
+        private System.Windows.Forms.Button buttonThanhtoan;
+        private System.Windows.Forms.ComboBox cbGiamgia;
+        private System.Windows.Forms.NumericUpDown nrSoluong;
+        private System.Windows.Forms.Button buttonThemmon;
+        private System.Windows.Forms.ComboBox comboBoxMon;
+        private System.Windows.Forms.FlowLayoutPanel flpTable;
+        private System.Windows.Forms.ComboBox comboBoxLoai;
+        private System.Windows.Forms.GroupBox groupBoxBan;
+    }
+}
